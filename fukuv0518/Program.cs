14902ea1dfc2fd05a634778287d481a1049cd492
Sugimoto <sugimotodat@gmail.com>
2023-05-18 13:16:11 +0900

    Initial commit

---
+++ fukuv0518/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fukuv0518
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
