c5834900cd5c998e8b114a15257d54b417d3e7ea
Sugimoto <sugimotodat@gmail.com>
2023-05-18 13:28:27 +0900

    諸々追加

--- fukuv0518/Form1.cs
+++ fukuv0518/Form1.cs
@@ -6,5 +6,21 @@ namespace fukuv0518
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Left = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "êôñ{Å@èÆãP";
+
+        }
     }
 }
